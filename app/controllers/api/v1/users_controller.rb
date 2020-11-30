8d4cff36ed7bdea55f0793739315ddf1843ba6c0
github username <meliajm@gmail.com>
2020-11-30 11:29:40 -0600

    post and notification list and user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,10 +12,27 @@ class Api::V1::UsersController < ApplicationController
         render json: @user
     end
 
+
+    # POST /users
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else 
+            render error: { error : 'Unable to make a user.' }, status: 400
+        end 
+    end
+
+    #???
+    def notifications
+        @users = User.receivesNotifications
+        render json: @users
+    end
+
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:firstname, :lastname, :phonenumber, :email, :receivesNotifications)
     end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+liza = User.create( firstname: 'LizaMarie', lastname: 'Shorty', phonenumber: '123456789', receivesNotification: true, email: "lizamarie@email.com")
