4740d2fca7764ec3e6f96fdb61ad5f31a7a5a678
github username <meliajm@gmail.com>
2020-11-30 15:39:59 -0600

    remove post to see if can get

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,14 +14,14 @@ class Api::V1::UsersController < ApplicationController
 
 
     # POST /users
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: @user
-        else 
-            render error: { error : 'Unable to make a user.' }, status: 400
-        end 
-    end
+    # def create
+    #     @user = User.new(user_params)
+    #     if @user.save
+    #         render json: @user
+    #     else 
+    #         render error: { error : 'Unable to make a user.' }, status: 400
+    #     end 
+    # end
 
     #???
     # def notifications
